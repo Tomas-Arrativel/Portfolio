cf1d3f0cbf1f3ec4b7cb1da743cf91a503d81620
Tomás <arrativeltomas@gmail.com>
2024-09-30 15:47:07 -0300

    Adding a new project to the portfolio

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,59 +1,59 @@
-import React from 'react';
-import { Forms } from '../../components/exports';
-import { FaInstagram, FaLinkedin, FaMailBulk } from 'react-icons/fa';
-import './contact.css';
+import React from "react";
+import { Forms } from "../../components/exports";
+import { FaInstagram, FaLinkedin, FaMailBulk } from "react-icons/fa";
+import "./contact.css";
 
 const Contact = () => {
-  return (
-    <div className='contact__container section__padding' id='contact'>
-      <h2>Contact</h2>
-      <h3>If I can be of assistance, please do not hesitate to contact me.</h3>
-      <div className='contact__forms'>
-        <Forms
-          icon={
-            <FaInstagram
-              style={{
-                height: '40px',
-                width: '40px',
-                color: 'var(--dark)',
-              }}
-            />
-          }
-          text='Instagram'
-          name='arrativel.tomas'
-          link='https://www.instagram.com/arrativel.tomas/'
-        />
-        <Forms
-          icon={
-            <FaLinkedin
-              style={{
-                height: '40px',
-                width: '40px',
-                color: 'var(--dark)',
-              }}
-            />
-          }
-          text='LinkedIn'
-          name='Tomás Arrativel'
-          link='https://www.linkedin.com/in/juan-tom%C3%A1s-arrativel-59281a240/'
-        />
-        <Forms
-          icon={
-            <FaMailBulk
-              style={{
-                height: '40px',
-                width: '40px',
-                color: 'var(--dark)',
-              }}
-            />
-          }
-          text='E-mail'
-          name='arrativeltomas@gmail.com'
-          link='#'
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="contact__container section__padding" id="contact">
+			<h2>Contact</h2>
+			<h3>If I can be of assistance, please do not hesitate to contact me.</h3>
+			<div className="contact__forms">
+				<Forms
+					icon={
+						<FaInstagram
+							style={{
+								height: "40px",
+								width: "40px",
+								color: "var(--dark)",
+							}}
+						/>
+					}
+					text="Instagram"
+					name="arrativeltomas"
+					link="https://www.instagram.com/arrativeltomas/"
+				/>
+				<Forms
+					icon={
+						<FaLinkedin
+							style={{
+								height: "40px",
+								width: "40px",
+								color: "var(--dark)",
+							}}
+						/>
+					}
+					text="LinkedIn"
+					name="Tomás Arrativel"
+					link="https://www.linkedin.com/in/juan-tom%C3%A1s-arrativel-59281a240/"
+				/>
+				<Forms
+					icon={
+						<FaMailBulk
+							style={{
+								height: "40px",
+								width: "40px",
+								color: "var(--dark)",
+							}}
+						/>
+					}
+					text="E-mail"
+					name="arrativeltomas@gmail.com"
+					link="#"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,47 +1,62 @@
-import React from 'react';
-import './projects.css';
-import { Project } from '../../components/exports';
+import React from "react";
+import "./projects.css";
+import { Project } from "../../components/exports";
 
 const Projects = () => {
-  return (
-    <div className='main__projects section__padding' id='projects'>
-      <h2 className='title'>Projects</h2>
-      <div className='main__projects-container'>
-        <Project
-          img='img/Simongame.PNG'
-          title='Simon Game'
-          text='This project is a challenge of a course that i made were they want you to do a game of memory called Simon game. Here you have to touch the corresponding button. To know which button you have to touch next, you will see and hear a button that you will have to memorize and add to the rest of the buttons that the game have been telling you.'
-          technologies={['HTML', 'CSS', 'Javascript', 'JQuery']}
-          link='https://simon-game-tomas.netlify.app/'
-          srcLink='https://github.com/Tomas-Arrativel/Web-development/tree/main/Simon%20Game%20Challenge%20Starting%20Files'
-        />
-        <Project
-          img='img/mxarg.PNG'
-          title='MX-Arg'
-          text='This is a page that simulates a rental webpage of tracks and bikes for motocross based in Argentina.'
-          technologies={['HTML', 'CSS', 'Javascript', 'ReactJs']}
-          link='https://mxarg.vercel.app/'
-          srcLink='https://github.com/Tomas-Arrativel/portfolio-pages/tree/main/MX-Arg'
-        />
-        <Project
-          img='img/paletteheaven.PNG'
-          title='Palette Heaven'
-          text="This is a personal project that I created for selling websites to house-painting companies. Here, you can explore various features, including a form that sends me an email with the user's input, animated dropdown menus, and much more."
-          technologies={['HTML', 'CSS', 'TypeScript', 'ReactJs']}
-          link='https://palette-heaven.vercel.app/'
-          srcLink='https://github.com/Tomas-Arrativel/PaletteHaven'
-        />
-        <Project
-          img='img/skycommerce.PNG'
-          title='SkyCommerce'
-          text='In this project you will find an ecommerce where you can add products to the cart, search products and more, I made this by using an api for the products and react-router-dom to navigate through the pages.'
-          technologies={['HTML', 'CSS', 'React', 'Typescript', 'API']}
-          link='https://sky-commerce.vercel.app/'
-          srcLink='https://github.com/Tomas-Arrativel/SkyCommerce'
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="main__projects section__padding" id="projects">
+			<h2 className="title">Projects</h2>
+			<div className="main__projects-container">
+				<Project
+					img="img/mxarg.PNG"
+					title="MX-Arg"
+					text="This is a page that simulates a rental webpage of tracks and bikes for motocross based in Argentina."
+					technologies={["Front-end", "HTML", "CSS", "Javascript", "ReactJs"]}
+					link="https://mxarg.vercel.app/"
+					srcLink="https://github.com/Tomas-Arrativel/portfolio-pages/tree/main/MX-Arg"
+				/>
+				<Project
+					img="img/paletteheaven.PNG"
+					title="Palette Heaven"
+					text="This is a personal project that I created for selling websites to house-painting companies. Here, you can explore various features, including a form that sends me an email with the user's input, animated dropdown menus, and much more."
+					technologies={["Front-end", "HTML", "CSS", "TypeScript", "ReactJs"]}
+					link="https://palette-heaven.vercel.app/"
+					srcLink="https://github.com/Tomas-Arrativel/PaletteHaven"
+				/>
+				<Project
+					img="img/skycommerce.PNG"
+					title="SkyCommerce"
+					text="In this project you will find an ecommerce where you can add products to the cart, search products and more, I made this by using an api for the products and react-router-dom to navigate through the pages."
+					technologies={[
+						"Front-end",
+						"HTML",
+						"CSS",
+						"React",
+						"Typescript",
+						"API",
+					]}
+					link="https://sky-commerce.vercel.app/"
+					srcLink="https://github.com/Tomas-Arrativel/SkyCommerce"
+				/>
+				<Project
+					img="img/ecommerce-fullstack.PNG"
+					title="Ecommerce Fullstack"
+					text="This is my first full-stack project, an ecommerce platform where users can create an account, add products to their cart, filter by category, view orders, remove items from the cart, and more. I plan to deploy this project soon so it can be accessed from any device."
+					technologies={[
+						"Full-stack",
+						"React",
+						"Javascript",
+						"API",
+						"NodeJS",
+						"ExpressJS",
+						"PostgreSQL",
+					]}
+					link="https://www.youtube.com/watch?v=8vte6944dTI"
+					srcLink="https://github.com/Tomas-Arrativel/Ecommerce-Codecademy"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Projects;
